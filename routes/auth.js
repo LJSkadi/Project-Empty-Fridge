6c6b9e8ca83f15bf4ea63ad9e37380e0633597f1
silvio-galli <silvio.galli@gmail.com>
2018-06-24 19:17:25 +0200

    Fixed POST /signup position in routes

--- routes/auth.js
+++ routes/auth.js
@@ -7,43 +7,11 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
+//#region GET /signup
 authRoutes.get("/signup", (req, res, next) => {
   res.render("users/signup");
 });
-
-authRoutes.get("/login", (req, res, next) => {
-  res.render("users/login", { "message": req.flash("error") });
-});
-
-authRoutes.post("/login", passport.authenticate("local", {
-  //successRedirect: "/", we overwrite this Passport command to redirect logged in user where we want
-  failureRedirect: "/login",
-  failureFlash: true,
-  passReqToCallback: true
-}), (req, res, next) => {
-  const userId = req.user._id;
-  res.redirect(`/user/${userId}`); //  we redirect logged in user to /user/userId (his/her profile)
-});
-
-authRoutes.use( (req, res, next) => {
-  if ( req.isAuthenticated() ) {
-    console.log( "YES YOU ARE LOGGED IN" );
-    next();
-  } else {
-    console.log("YOU MUST LOG IN BEFORE GOING TO USER PAGE");
-    res.redirect('/login');
-    return;
-  }
-})
-
-authRoutes.get('/user/:userId',(req, res, next) => {
-  User.findOne( { "_id": req.params.userId } )
-  .then( user => {
-    res.render( 'users/profile', user);
-  } )
-  .catch( err => { throw err } );
-});
+//#endregion
 
 
 authRoutes.post("/signup", (req, res, next) => {
@@ -79,6 +47,42 @@ authRoutes.post("/signup", (req, res, next) => {
     });
   });
 });
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("users/login", { "message": req.flash("error") });
+});
+
+
+authRoutes.post("/login", passport.authenticate("local", {
+  //successRedirect: "/", we overwrite this Passport command to redirect logged in user where we want
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}), (req, res, next) => {
+  const userId = req.user._id;
+  res.redirect(`/user/${userId}`); //  we redirect logged in user to /user/userId (his/her profile)
+});
+
+authRoutes.use( (req, res, next) => {
+  if ( req.isAuthenticated() ) {
+    console.log( "YES YOU ARE LOGGED IN" );
+    next();
+  } else {
+    console.log("YOU MUST LOG IN BEFORE GOING TO USER PAGE");
+    res.redirect('/login');
+    return;
+  }
+})
+
+authRoutes.get('/user/:userId',(req, res, next) => {
+  User.findOne( { "_id": req.params.userId } )
+  .then( user => {
+    res.render( 'users/profile', user);
+  } )
+  .catch( err => { throw err } );
+});
+
+
 
 authRoutes.get("/logout", (req, res) => {
   req.logout();
