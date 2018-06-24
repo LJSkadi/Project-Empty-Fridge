f9975033c0da2909d3a841ccda1c9a7c9c46bf55
silvio-galli <silvio.galli@gmail.com>
2018-06-24 16:43:32 +0200

    Back to previous /auth prefix in addresses

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ authRoutes.get("/login", (req, res, next) => {
 });
 
 authRoutes.post("/login", passport.authenticate("local", {
-  successRedirect: "/user",
+  successRedirect: "/",
   failureRedirect: "/auth/login",
   failureFlash: true,
   passReqToCallback: true
