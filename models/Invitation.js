af3a38f19d9c1d8427ebf9a39016fe11f82814aa
LJ Skadi <LJ.Skadi@gmail.com>
2018-06-27 12:46:03 +0200

    Elliot - deleted double references in the Invitation Schema

--- models/Invitation.js
+++ models/Invitation.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const   invitationSchema = new Schema({
-  _sendingUser:    { 
-      _id:      {type: Schema.Types.ObjectId, ref:'User'},
-      username: {type: String}
-    },
-  _receivingUser:  {
-      _id:      {type: Schema.Types.ObjectId, ref: 'User'},
-      username: {type: String}
-  },
+   receivingUser:  {
+    username:   {type: String},
+    id:        {type: String},
+    email:      {type: String}, 
+    required: true},
   _list: {type: Schema.Types.ObjectId , ref: 'List', required: true},
   confirmationCode: String,
   refuseCode: String,
