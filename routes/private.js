9ab9bcb238621963fe67806f8caf0b80a12e9b50
LJ Skadi <LJ.Skadi@gmail.com>
2018-06-27 09:10:54 +0200

    Elliot - added Bootstrap to the package.json, added a delete button for the list and delete function for a list  which is only useable for the owner, added some stuff for the design

--- routes/private.js
+++ routes/private.js
@@ -17,7 +17,6 @@ privateRoutes.get('/user/:userId',(req, res, next) => {
     List.find( { _creator : req.params.userId })]
   )
   .then( ( array ) => {
-    console.log(array);
     res.render( 'users/profile', { user: array[0], lists: array[1]} );
   } )
   .catch( err => { throw err } );
@@ -69,7 +68,6 @@ privateRoutes.get('/list/:listId', (req, res, next) => {
   .then(list => {
     const openItems = list._items.filter((item) => item.status ==='OPEN')
     const closedItems = list._items.filter((item) => item.status ==='CLOSED')
-    console.log( "These are the open items", openItems);
     res.render('lists/list-details', {list: list, openItems: openItems, closedItems:closedItems} )
   })
   .catch( err => { throw err })
@@ -106,8 +104,6 @@ privateRoutes.get('/delete-item/:itemId', (req, res, next) => {
   Item.findByIdAndUpdate(req.params.itemId, { status: 'CLOSED' }, {new: true})
   .populate('_list')
   .then( updatedItem => {
-    console.log( "This is the closed Item" , updatedItem );
-    //console.log(`${updatedItem._list._id}`);
     res.redirect(`/list/${updatedItem._list._id}`)
   })
   .catch( err => { throw err } )
@@ -118,8 +114,6 @@ privateRoutes.get('/reactivate-item/:itemId', (req, res, next) => {
   Item.findByIdAndUpdate(req.params.itemId, { status: 'OPEN' }, {new: true})
   .populate('_list')
   .then( updatedItem => {
-    console.log( "This is the reactivated Item" , updatedItem );
-    //console.log(`${updatedItem._list._id}`);
     res.redirect(`/list/${updatedItem._list._id}`)
   })
   .catch( err => { throw err } )
@@ -132,4 +126,54 @@ privateRoutes.get("/logout", (req, res) => {
 });
 //#endregion
 
+//#region GET/delete-list
+privateRoutes.get('/delete-list/:listId', (req, res, next) => {
+  let userId = req.user._id;
+  let listId = req.params.listId;
+  List.findById(listId)
+  .then( list => {
+    if (`${userId}` == `${list._creator}`){
+      Item.deleteMany({_list: listId})
+      .then( itemsDeleted => {
+        List.findByIdAndRemove(req.params.listId)
+        .then( listToDelete => {
+          let message = "Your list is deleted successfully";
+          
+          res.redirect(`/user/${userId}`)
+        })
+      })
+      .catch( err => { throw err } )
+    }else{
+      res.redirect(`/user/${userId}`)
+    }
+  })
+  .catch( err => { throw err } )
+})
+
 module.exports = privateRoutes;
+
+// function checkCreator(userId, element) {
+//   return function(req, res, next) {
+//     if (req.isAuthenticated() && element._creator === userId ) {
+//       return next();
+//     }
+//   }
+// }
+
+// const checkListCreator  = checkCreator(req.user._id, list);
+// const checkItemCreator = checkCreator(req.user._id, item);
+
+// //* GET method to show the signup form
+// privateRoutes.get('/delete-list/:listId', checkListCreator, (req, res, next) => {
+//   Item.deleteMany({_list: req.params.listId})
+//   .then( itemsDeleted => {
+//     console.log("I'm here")
+//     List.findByIdAndRemove(req.params.listId)
+//     .then( listToDelete => {
+//       let message = "Your list is deleted successfully";
+//       let userId = req.user._id;
+//       res.redirect(`/user/${userId}`)
+//     })
+//   })
+//   .catch( err => { throw err } )
+// })
